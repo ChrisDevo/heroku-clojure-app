b1cc7916261dbef69366fca2f7ac23f0b890fb52
Chris Devine <cdevine@annadaletech.com>
2013-03-15 10:07:03 +0000

    Initial commit

---
+++ test/heroku_clojure_app/web_test.clj
@@ -0,0 +1,6 @@
+(ns heroku-clojure-app.web-test
+  (:require [clojure.test :refer :all]
+            [heroku-clojure-app.web :refer :all]))
+
+(deftest first-test
+  (is false "Tests should be written"))
